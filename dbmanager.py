89ea09c985aa63d6d121d29a21c7a410d4f1932b
jovistar <jovistar@gmail.com>
2014-03-24 08:18:53 -0400

    Add DB Init

--- dbmanager.py
+++ dbmanager.py
@@ -16,3 +16,21 @@ def open_db_con():
 	
 	dbCon = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name, charset='utf8')
 	return dbCon
+
+def create_db():
+	cf = ConfigParser.ConfigParser()
+	cf.read('ncd.cnf')
+
+	db_host = cf.get('db', 'db_host')
+	db_user = cf.get('db', 'db_user')
+	db_pass = cf.get('db', 'db_pass')
+	db_name = cf.get('db', 'db_name')
+
+	dbCon = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, charset="utf8")
+	cursor = dbCon.cursor()
+	cursor.execute('CREATE DATABASE IF NOT EXISTS %s' % db_name)
+	cursor.select_db(db_name)
+
+
+
+
